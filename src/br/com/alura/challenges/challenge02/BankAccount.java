f1918c1acc0020c042a0571401c3e58198b2004a
athena272 <guilhermera272@gmail.com>
2025-01-30 13:27:35 -0300

    Create BankAccount.java

---
+++ src/br/com/alura/challenges/challenge02/BankAccount.java
@@ -0,0 +1,31 @@
+package br.com.alura.challenges.challenge02;
+
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private String holder;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+}
