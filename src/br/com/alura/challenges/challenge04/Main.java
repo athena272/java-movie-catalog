3fe93bbee397c1c201063c0cdd64c3242f21ec3e
athena272 <guilhermera272@gmail.com>
2025-01-31 13:23:21 -0300

    Create Main.java

---
+++ src/br/com/alura/challenges/challenge04/Main.java
@@ -0,0 +1,10 @@
+package br.com.alura.challenges.challenge04;
+
+import br.com.alura.challenges.challenge04.models.CurrencyConverter;
+
+public class Main {
+    public static void main(String[] args) {
+        CurrencyConverter converter = new CurrencyConverter();
+        converter.convertDollarToReal(50);
+    }
+}
