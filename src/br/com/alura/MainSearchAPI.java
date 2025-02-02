c46bb1993226fc9629772fd1785c36212c9661aa
athena272 <guilhermera272@gmail.com>
2025-02-02 03:15:32 -0300

    Update MainSearchAPI.java

--- src/br/com/alura/MainSearchAPI.java
+++ src/br/com/alura/MainSearchAPI.java
@@ -1,12 +1,16 @@
 package br.com.alura;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class MainSearchAPI {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.omdbapi.com/?t=inside+out&apikey=6124c0b2")).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
