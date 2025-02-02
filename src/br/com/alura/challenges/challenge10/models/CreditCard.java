94b00a2296a7ef01670f8a05ec3acb74b109d6e6
athena272 <guilhermera272@gmail.com>
2025-02-02 01:47:36 -0300

    Create CreditCard.java

---
+++ src/br/com/alura/challenges/challenge10/models/CreditCard.java
@@ -0,0 +1,37 @@
+package br.com.alura.challenges.challenge10.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+    private double limit;
+    private double balance;
+    private List<Purchase> purchases;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.balance = limit;
+        this.purchases = new ArrayList<>();
+    }
+
+    public boolean addPurchase(Purchase purchase) {
+        if (this.balance > purchase.getValue()) {
+            this.balance = this.balance - purchase.getValue();
+            this.purchases.add(purchase);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+}
