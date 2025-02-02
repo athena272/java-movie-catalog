921df2aeb7259256cba73203052a05d6de2e36aa
athena272 <guilhermera272@gmail.com>
2025-02-02 03:00:50 -0300

    Update MainSearchAPI.java

--- src/br/com/alura/MainSearchAPI.java
+++ src/br/com/alura/MainSearchAPI.java
@@ -1,7 +1,12 @@
 package br.com.alura;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class MainSearchAPI {
     public static void main(String[] args) {
-        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://foo.com/")).build();
     }
 }
