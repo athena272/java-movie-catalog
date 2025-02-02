ec2cc39a0aace4ac1c6b27019bf1ee93f7c4de50
athena272 <guilhermera272@gmail.com>
2025-02-02 11:04:27 -0300

    Update MainSearchAPI.java

--- src/br/com/alura/MainSearchAPI.java
+++ src/br/com/alura/MainSearchAPI.java
@@ -5,11 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearchAPI {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter search movie: ");
+        var search = input.nextLine();
+
+        String urlToUse = "https://www.omdbapi.com/?t=" + search + "&apikey=6124c0b2";
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.omdbapi.com/?t=inside+out&apikey=6124c0b2")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlToUse)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
